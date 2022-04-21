2b9e657051b7dfdb3ddd21df0a5fd83c9967ecac
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-22 01:02:36 +0200

    the grayed out part in App.jsx is for useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Trivia from "./components/trivia/Trivia";
 import { dataQuetionaire, moneyPyramid } from "../src/dataSource";
@@ -7,6 +7,26 @@ function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("R 0");
+
+  // const moneyPyramid = useMemo(() => [
+  //     { id: 1, amount: "R 100" },
+  //     { id: 2, amount: "R 200" },
+  //     { id: 3, amount: "R 1000" },
+  //     { id: 4, amount: "R 2500" },
+  //     { id: 5, amount: "R 5000" },
+  //     { id: 6, amount: "R 10 000" },
+  //     { id: 7, amount: "R 15 000" },
+  //     { id: 8, amount: "R 40 000" },
+  //     { id: 9, amount: "R 50 000" },
+  //     { id: 10, amount: "R 90 000" },
+  //     { id: 11, amount: "R 100 000" },
+  //     { id: 12, amount: "R 150 000" },
+  //     { id: 13, amount: "R 200 000" },
+  //     { id: 14, amount: "R 580 000" },
+  //     { id: 15, amount: "R 1 000 000" },
+  //   ].reverse(), 
+  //   []
+  // ); 
 
   useEffect(() =>{
     questionNumber >1 && setEarned(moneyPyramid.find((money)=> money.id === questionNumber - 1).amount);
