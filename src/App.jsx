9c7234a2db176876233786e1e5abdb37e31c0d00
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2022-04-22 00:56:10 +0200

    added useState for money earned to make use of in losing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Trivia from "./components/trivia/Trivia";
 import { dataQuetionaire, moneyPyramid } from "../src/dataSource";
@@ -6,21 +6,32 @@ import { dataQuetionaire, moneyPyramid } from "../src/dataSource";
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
   const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("R 0");
+
+  useEffect(() =>{
+    questionNumber >1 && setEarned(moneyPyramid.find((money)=> money.id === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
 
   return (
     <div className="app">
       <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-          <Trivia 
-            dataQuetionaire={dataQuetionaire} 
-            setStop={setStop}
-            questionNumber={questionNumber} 
-            setQuestionNumber={setQuestionNumber} 
-          />
-        </div>
+        {stop ? ( 
+          <h1 className="endText">You earned: {earned}</h1> 
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">30</div>
+            </div>
+            <div className="bottom">
+              <Trivia 
+                dataQuetionaire={dataQuetionaire} 
+                setStop={setStop}
+                questionNumber={questionNumber} 
+                setQuestionNumber={setQuestionNumber} 
+              />
+            </div>
+          </>
+        )}
       </div>
       <div className="pyramid">
         <ul className="moneyList">
